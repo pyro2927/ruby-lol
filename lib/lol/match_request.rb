04c6c7b06411e4843542049a5fb252006047efba
Joseph Pintozzi <joseph@pintozzi.com>
2016-01-14 15:53:59 -0600

    Adding get_tournament method
    Allowing matches for tournaments to be fetched

--- lib/lol/match_request.rb
+++ lib/lol/match_request.rb
@@ -12,5 +12,13 @@ module Lol
     def get match_id
       perform_request(api_url("match/#{match_id}"))
     end
+
+    # Returns a match with the given id
+    # @param tournament_code [String] Tournament Code of the Match
+    # @param match_id [Fixnum] Match ID
+    # @return [Hash] match object
+    def get_tournament tournament_code, match_id
+      perform_request(api_url("match/for-tournament/#{match_id}", { tournamentCode: tournament_code }))
+    end
   end
 end
